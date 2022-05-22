a9b8fb22150668f7f7d11a606953b799bd103d13
Harish B <dev.harish@outlook.com>
2022-05-22 09:16:21 +0530

    Array Problems 5.5, 5.6

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -3,13 +3,64 @@ from typing import List
 from test_framework import generic_test
 
 
+# Approach 1: Naive Solution
+# Test Case #40x took HUGE time.
 def buy_and_sell_stock_once(prices: List[float]) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    '''T=O(n^2); S=O(1)'''
+    max_profit = 0.0
+    for i in range(len(prices)-1):
+        bought = prices[i]
+        sold = max(prices[i+1:])
+        max_profit = max(max_profit, sold - bought)
+    return max_profit
 
+# Approach 2: Linear Time and Space
+# Test Case #40x took LESS time now.
+def buy_and_sell_stock_once(prices: List[float]) -> float:
+    '''T=O(n); S=O(n)'''
+    max_sell_array = [0] * len(prices)
+    for i in range(len(prices)-2, -1, -1):
+        max_sell_array[i] = max(prices[i+1], max_sell_array[i+1])
+    
+    max_profit = 0
+    for i in range(len(prices)):
+        max_profit = max(max_profit, max_sell_array[i]-prices[i])
+    
+    return max_profit
+
+# Approach 3: Constant Space
+def buy_and_sell_stock_once(prices: List[float]) -> float:
+    '''T=O(n); S=O(1)'''
+    max_sell, max_profit = 0, 0
+    for i in range(len(prices)-2, -1, -1):
+        max_sell = max(max_sell, prices[i+1])
+        buy = prices[i]
+        profit = max_sell - buy
+        max_profit = max(profit, max_profit)
+    
+    return max_profit
+
+# Book Solution
+# Use minimum price seen so far and use current price
+# to compute the profit if we want to sell at current price.
+def buy_and_sell_stock_once(prices: List[float]) -> float:
+    min_price_seen_so_far, max_profit = float('inf'), 0.0
+    for price in prices:
+        max_profit_sell_today = price - min_price_seen_so_far
+        max_profit = max(max_profit, max_profit_sell_today)
+        min_price_seen_so_far = min(min_price_seen_so_far, price)
+    
+    return max_profit
 
 if __name__ == '__main__':
     exit(
         generic_test.generic_test_main('buy_and_sell_stock.py',
                                        'buy_and_sell_stock.tsv',
                                        buy_and_sell_stock_once))
+
+# Variant
+
+'''
+Write a program that takes an array of integers and finds
+the length of a longest subarray all of whose entries are equal.
+'''
--- epi_judge_python/sorted_array_remove_dups.py
+++ epi_judge_python/sorted_array_remove_dups.py
@@ -5,9 +5,9 @@ from test_framework import generic_test
 from test_framework.test_utils import enable_executor_hook
 
 
-# Returns the number of valid entries after deletion.
+# Approach 1: Naive Solution
 def delete_duplicates(A: List[int]) -> int:
-    '''Naive Solution: T=O(n); S=O(n)'''
+    '''T=O(n); S=O(n)'''
     if len(A) == 0:
         return 0
 
@@ -19,12 +19,34 @@ def delete_duplicates(A: List[int]) -> int:
             result.append(A[i])
     
     num_valid = len(result)
-    for i in range(len(A)-num_valid):
-        result.append(0)
     
     A[:] = result[:]
     return num_valid
 
+# Approach 2: Constant Space
+def delete_duplicates(A: List[int]) -> int:
+    '''T=O(n); S=O(1)'''
+    unique = 0
+    for i in range(1, len(A)):
+        if A[i] != A[unique]:
+            unique += 1
+            A[unique] = A[i]
+    
+    num_valid = unique + 1
+    return num_valid
+
+# Book Solution
+def delete_duplicates(A: List[int]) -> int:
+    '''T=O(n); S=O(1)'''
+    if not A:
+        return 0
+    
+    write_index = 1
+    for i in range(1, len(A)):
+        if A[write_index - 1] != A[i]:
+            A[write_index] = A[i]
+            write_index += 1
+    return write_index
 
 @enable_executor_hook
 def delete_duplicates_wrapper(executor, A):
@@ -37,3 +59,20 @@ if __name__ == '__main__':
         generic_test.generic_test_main('sorted_array_remove_dups.py',
                                        'sorted_array_remove_dups.tsv',
                                        delete_duplicates_wrapper))
+
+# Variant
+
+'''
+Implement a function which takes as input an array and a key, and updates the
+array so that all occurrences of the input key have been removed and the
+remaining elements have been shifted left to fill the emptied indices. Return
+the number of remaining elements. There are no requirements as to the values
+stored beyond the last valid element.
+'''
+
+'''
+Write a program which takes as input a sorted array A of integers and a
+positive integer m, and updates A so that if x appears m times in A it
+appears exactly min(2, m) times in A. The update to A should be performed
+in one pass, and no additional storage may be allocated.
+'''
