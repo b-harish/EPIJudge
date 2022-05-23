e63558b11b7295ede4ff96d0af68ee557dd15d7e
Harish B <dev.harish@outlook.com>
2022-05-23 10:04:55 +0530

    Array Problems 5.7 (Naive)

--- epi_judge_python/buy_and_sell_stock_twice.py
+++ epi_judge_python/buy_and_sell_stock_twice.py
@@ -2,10 +2,31 @@ from typing import List
 
 from test_framework import generic_test
 
+def buy_and_sell_stock_once(prices: List[float]) -> float:
+    max_sell, max_profit = 0, 0
+    for i in range(len(prices)-2, -1, -1):
+        max_sell = max(max_sell, prices[i+1])
+        buy = prices[i]
+        profit = max_sell - buy
+        max_profit = max(profit, max_profit)
+    
+    return max_profit
 
+# Keeps on running at [400/402] test case. Halted the program.
 def buy_and_sell_stock_twice(prices: List[float]) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    '''T=O(n^2); S=O(1)'''
+    if not prices:
+        return 0
+
+    max_profit = 0
+    # +1 in the for loop allows for  doing a single transaction from start to end
+    for i in range(1, len(prices)+1):
+        profit1 = buy_and_sell_stock_once(prices[:i])
+        profit2 = buy_and_sell_stock_once(prices[i:])
+        profit = profit1 + profit2
+        max_profit = max(profit, max_profit)
+
+    return max_profit
 
 
 if __name__ == '__main__':
